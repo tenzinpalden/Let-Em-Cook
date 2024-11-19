9cb442959647e552479b1f84c49fbf9026066cb9
BensonCheng8787 <bensoncheng888@gmail.com>
2024-11-19 12:55:34 -0500

    additional formatting fixes

--- letemcook/src/pages/contact.jsx
+++ letemcook/src/pages/contact.jsx
@@ -4,21 +4,23 @@ import './ContactPage.css'; // CSS file for styling
 const SimpleContactPage = () => {
   const members = [
     { name: "Tyler John Layton", email: "laytot@rpi.edu", image: "/members/tyler.png" },
-    { name: "Nancy John Zhang", email: "zhangn3@rpi.edu", image: "/members/nancy.png" },
-    { name: "Annabel Jong Choi", email: "choia7@rpi.edu", image: "/members/annab.png" },
+    { name: "Nancy John Zhang", email: "zhangn7@rpi.edu", image: "/members/nancy.png" },
+    { name: "Annabel JongJohnJohnjoHNLinLin Choi", email: "choia7@rpi.edu", image: "/members/annab.png" },
     { name: "Benson John Cheng", email: "chengb2@rpi.edu", image: "/members/benson.png" },
-    { name: "Tenzen John Palden", email: "tpalden@rpi.edu", image: "/members/tp.png" },
+    { name: "Tenzin John Palden", email: "tpalden@rpi.edu", image: "/members/tp.png" },
   ];
 
   return (
     <div className="simple-contact-page">
-      <h1 className="contact-title">Contact Us</h1>
+    <h1 className="contact-title">Contact Us</h1>
+    <div className="contact-members-container">
       {members.map((member, index) => (
         <div className="contact-member" key={index}>
-          <div
-            className="contact-image-container"
-            style={{ backgroundImage: `url(${member.image})` }}
-          ></div>
+          <img
+            src={member.image}
+            alt={`${member.name}'s profile`}
+            className="contact-image"
+          />
           <h2 className="contact-name">{member.name}</h2>
           <a href={`mailto:${member.email}`} className="email-link">
             {member.email}
@@ -26,6 +28,7 @@ const SimpleContactPage = () => {
         </div>
       ))}
     </div>
+  </div>
   );
 };
 
